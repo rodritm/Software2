b3f1120a4383f5627aded2052a2968ec4881fd89
rodritm <rodritm2010@gmail.com>
2018-11-25 18:17:52 -0400

    First

---
+++ src/Prueba.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "esto es una prueba de integracion");
+	}
+
+}
+
